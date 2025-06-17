de8e9f7556dc3afd7a2a07b2948b23a4b4bcf7f6
Kuppur Ravindranatha <vinaykr0605@gmail.com>
2025-06-17 16:08:22 +0530

    image upload .NET7 initial commit

---
+++ NGINX-Image-Upload-App/ImageApp/ImageApp/ImageHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+public static class ImageHelper
+{
+    public static byte[] ConvertImageToBytes(string imagePath)
+    {
+        using (var image = Image.FromFile(imagePath))
+        using (var stream = new MemoryStream())
+        {
+            image.Save(stream, image.RawFormat);
+            return stream.ToArray();
+        }
+    }
+}
---
+++ NGINX-Image-Upload-App/ImageApp/ImageApp/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string sourceDirectory = @"NGINX-Image-Upload-App\imageSource";
+        string destinationDirectory = @"NGINX-Image-Upload-App\imageDestination";
+
+
+
+        string[] imageExtensions = { ".jpg", ".jpeg", ".png" };
+
+        foreach (string file in Directory.EnumerateFiles(sourceDirectory))
+        {
+            if (imageExtensions.Contains(Path.GetExtension(file).ToLower()))
+            {
+                string destinationFileName = Path.Combine(destinationDirectory, Path.GetFileName(file));
+
+                if (!File.Exists(destinationFileName))
+                {
+                    byte[] imageBytes = ImageHelper.ConvertImageToBytes(file);
+                    File.WriteAllBytes(destinationFileName, imageBytes);
+                    Console.WriteLine($"Image '{file}' converted and saved to '{destinationFileName}' " +
+                        $"with a size of {new FileInfo(destinationFileName).Length} bytes.");
+                }
+                else
+                {
+                    Console.WriteLine($"Image '{file}' already exists in the destination folder.");
+                }
+            }
+        }
+
+        Console.WriteLine("Image conversion and storage completed.");
+    }
+}
+
